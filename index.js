a979a23c319117fb2ffd8de6b856de0b3dbce92f
idcso <52316112+idcso@users.noreply.github.com>
2023-03-02 02:14:59 +0600

    Create function that plays one single round

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ function getComputerChoice() {
 
 	return gameChoices[Math.floor( Math.random() * gameChoices.length )];
 }
+
+function playRound(playerSelection, computerSelection) {
+	playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+	if (playerSelection === computerSelection) {
+		return 'Tie! Try again.';
+	} else if (playerSelection === 'Rock' && computerSelection === 'Scissors' ||
+	           playerSelection === 'Paper' && computerSelection === 'Rock' ||
+						 playerSelection === 'Scissors' && computerSelection === 'Paper') 
+	{
+		return `You win! ${playerSelection} beats ${computerSelection}`;
+	} else {
+		return `You lost! ${computerSelection} beats ${playerSelection}`;
+	}
+}
