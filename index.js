e81aa6080af81001ad65d21076e2687dfa9fb9ec
idcso <52316112+idcso@users.noreply.github.com>
2023-03-02 23:47:37 +0600

    Create main function game()
    Functions getPlayerChoice() and playRound() were also modified to be
    used in game() function.

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ function getComputerChoice() {
 	return gameChoices[Math.floor( Math.random() * gameChoices.length )];
 }
 
-function playRound(playerSelection, computerSelection) {
-	playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+function getPlayerChoice() {
+	let playerSelection = prompt('Choose: Rock / Paper / Scissors');
+	return playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+}
 
+function playRound(playerSelection, computerSelection) {
 	if (playerSelection === computerSelection) {
 		return 'Tie! Try again.';
 	} else if (playerSelection === 'Rock' && computerSelection === 'Scissors' ||
@@ -18,3 +21,33 @@ function playRound(playerSelection, computerSelection) {
 		return `You lost! ${computerSelection} beats ${playerSelection}`;
 	}
 }
+
+function game() {
+	let score = {
+		player: 0,
+		computer: 0
+	}
+
+	for (let i = 0; i < 5; i++) {
+		let result = playRound(getPlayerChoice(), getComputerChoice());
+		console.log(result);
+
+		if (result.includes('win')) {
+			score.player++;
+		} else if (result.includes('lost')) {
+			score.computer++;
+		} else {
+			i--;
+		}
+
+		console.log(`Score: ${score.player} - ${score.computer}`);
+	}
+
+	if (score.player > score.computer) {
+		console.log('You won!');
+	} else {
+		console.log('You lost!');
+	}
+}
+
+game();
