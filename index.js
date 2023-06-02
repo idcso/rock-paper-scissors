9f930f7fdf93e825eb347a5215d89354fabdb1f7
idcso <52316112+idcso@users.noreply.github.com>
2023-06-02 18:41:01 +0600

    Add simple UI

--- index.js
+++ index.js
@@ -1,53 +1,44 @@
+const buttons = document.querySelector('.buttons');
+const result = document.querySelector('.result');
+
+let gameScore = {
+	player: 0,
+	computer: 0
+}
+
+buttons.addEventListener('click', game);
+
 function getComputerChoice() {
 	const gameChoices = ['Rock', 'Paper', 'Scissors'];
 
 	return gameChoices[Math.floor( Math.random() * gameChoices.length )];
 }
 
-function getPlayerChoice() {
-	let playerSelection = prompt('Choose: Rock / Paper / Scissors');
-	return playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
-}
-
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection === computerSelection) {
-		return 'Tie! Try again.';
+		result.textContent = `Tie! Try again.\n Score: ${gameScore.player} - ${gameScore.computer}`;
 	} else if (playerSelection === 'Rock' && computerSelection === 'Scissors' ||
 	           playerSelection === 'Paper' && computerSelection === 'Rock' ||
 						 playerSelection === 'Scissors' && computerSelection === 'Paper') 
 	{
-		return `You win! ${playerSelection} beats ${computerSelection}`;
+		gameScore.player++;
+		result.textContent = `You win! ${playerSelection} beats ${computerSelection}\n Score: ${gameScore.player} - ${gameScore.computer}`;
 	} else {
-		return `You lost! ${computerSelection} beats ${playerSelection}`;
+		gameScore.computer++;
+		result.textContent = `You lost! ${computerSelection} beats ${playerSelection}\n Score: ${gameScore.player} - ${gameScore.computer}`;
 	}
 }
 
-function game() {
-	let score = {
-		player: 0,
-		computer: 0
-	}
+function game(event) {
+	if (gameScore.player < 5 && gameScore.computer < 5) {
+		playRound(event.target.textContent, getComputerChoice());
 
-	for (let i = 0; i < 5; i++) {
-		let result = playRound(getPlayerChoice(), getComputerChoice());
-		console.log(result);
-
-		if (result.includes('win')) {
-			score.player++;
-		} else if (result.includes('lost')) {
-			score.computer++;
-		} else {
-			i--;
+		if (gameScore.player === 5) {
+			result.textContent = `Game over! You won!\nScore: ${gameScore.player} - ${gameScore.computer}`;
+		} else if (gameScore.computer === 5) {
+			result.textContent = `Game over! You lost!\nScore: ${gameScore.player} - ${gameScore.computer}`;
 		}
-
-		console.log(`Score: ${score.player} - ${score.computer}`);
-	}
-
-	if (score.player > score.computer) {
-		console.log('You won!');
 	} else {
-		console.log('You lost!');
+		buttons.removeEventListener('click', game);
 	}
 }
-
-game();
